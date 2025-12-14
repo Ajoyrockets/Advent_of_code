f4bd2da77044150b0507c5d6c0448583871889c3
ajoyp <ajoypartapsingh@gmail.com>
2025-12-14 20:59:20 +0000

    new file:   Day_one_2025_files/Day_one_2025.py new file:   Day_one_2025_files/Day_one_2025_input_file.txt This is day one advent of code 2025 starting

---
+++ 2025/Day_one_2025_files/Day_one_2025.py
@@ -0,0 +1,12 @@
+start_vaule = 50
+
+with open(r"C:\Users\ajoyp\OneDrive\Documents\Advent_of_code\2025\Day_one_2025_files\Day_one_2025_input_file.txt", "r") as file:
+    List_input_file = [str(line.strip()) for line in file]
+    num_lines = len(List_input_file)
+    print(List_input_file)
+    print(num_lines)
+List_input_file_change = List_input_file 
+'''
+while num_lines > 0 :
+    List_input_file_change
+'''
