fbd18deb2d4d0740d5225fce7bac5fcd0e9a62da
ajoyp <ajoypartapsingh@gmail.com>
2025-12-15 21:12:35 +0000

    modified:   2025/Day_one_2025_files/Day_one_2025.py 	new file:   2025/Day_one_2025_files/Day_one_2025_test_file Improves the code and adds a test file

--- 2025/Day_one_2025_files/Day_one_2025.py
+++ 2025/Day_one_2025_files/Day_one_2025.py
@@ -1,12 +1,20 @@
 start_vaule = 50
-
+# This opens the input file 
 with open(r"C:\Users\ajoyp\OneDrive\Documents\Advent_of_code\2025\Day_one_2025_files\Day_one_2025_input_file.txt", "r") as file:
-    List_input_file = [str(line.strip()) for line in file]
-    num_lines = len(List_input_file)
-    print(List_input_file)
-    print(num_lines)
+    List_input_file = [str(line.strip()) for line in file] # This will turn it into a list 
+    num_lines = len(List_input_file) # how many rotations there are 
+
+List_input_file_change = []
 List_input_file_change = List_input_file 
-'''
-while num_lines > 0 :
-    List_input_file_change
-'''
+num_lines_change = num_lines
+while num_lines_change > 0 :
+    Next_rotation = []
+    Next_rotation = List_input_file_change.pop(0) # removes one item in the list at a time 
+    num_lines_change = len(List_input_file_change)
+    pos_neg = Next_rotation[0]
+    if pos_neg == "R" :                           # removes the letter to leave the number 
+        number_rotation = Next_rotation.lstrip("R")
+        print(number_rotation)
+    else :
+        number_rotation = Next_rotation.lstrip("L")
+        print(number_rotation)
